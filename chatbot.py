4c1345d30eca6a253f52360cabaffa2e8dfa07b1
Gustavocrz555 <102576894+Gustavocrz555@users.noreply.github.com>
2026-04-21 18:49:05 -0600

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -56,7 +56,7 @@ HTML_TEMPLATE = '''
 </head>
 <body>
     <div class="chat-container">
-        <h2>🤖 Chatbot Paralelo</h2>
+        <h2>chatbot</h2>
         <div id="chat-box">
             <div class="msg bot">¡Hola! Soy tu asistente de TICs. Pregúntame sobre Cómputo Paralelo.</div>
         </div>
